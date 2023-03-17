3ac4813e2d9885c17d803c8586510c6d0499168f
Thomas <91274184+Estaniss@users.noreply.github.com>
2023-03-17 14:12:14 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/**
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+import './src/utils/config/reactotron';
+import { AppRegistry } from 'react-native';
+import App from './scr';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
